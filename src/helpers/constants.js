2372d40a0d39bd4641b1c24b86852732e441d863
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-03-27 10:39:24 +0200

    update

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,5 +1,4 @@
 import { env } from '../utils/env.js';
-export const TELEGRAM_BOT_API = env('TG_TOKEN');
 export const ONE_HOUR = 60 * 60 * 1000;
 export const ONE_DAY = ONE_HOUR * 24;
 export const ONE_WEEK = ONE_DAY * 7;
--- src/helpers/response.js
+++ src/helpers/response.js
@@ -2,10 +2,10 @@ export function response(status) {
   return (data, headers = {}, options = {}) => {
     return {
       statusCode: status,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      multiValueHeaders: {
+        'Access-Control-Allow-Origin': ['*'],
+        'Access-Control-Allow-Headers': ['Content-Type, Authorization'],
+        'Access-Control-Allow-Methods': ['GET, POST, PUT, DELETE, OPTIONS'],
         ...headers,
       },
       body: JSON.stringify(data),
